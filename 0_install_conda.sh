ca2b2e0fff1feab0a9742dcdca0bec7a6d6d3f02
Rajendra Singh <singh.raj1997@gmail.com>
2023-09-22 13:19:53 +0530

    Create 0_install_conda.sh

---
+++ 0_install_conda.sh
@@ -0,0 +1,6 @@
+#Downloading the latest Miniconda installer for Linux. Your architecture may vary.
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+#eval "$(/Users/jsmith/miniconda/bin/conda shell.YOUR_SHELL_NAME hook)"
+#conda init
+#conda config --set auto_activate_base false
