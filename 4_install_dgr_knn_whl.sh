4fb19f6a88efc22b1414e08e2994fee95d9232cf
Rajendra Singh <singh.raj1997@gmail.com>
2023-09-22 13:12:54 +0530

    Update 4_install_dgr_knn_whl.sh

--- 4_install_dgr_knn_whl.sh
+++ 4_install_dgr_knn_whl.sh
@@ -1,2 +1,4 @@
 pip install -q https://huggingface.co/camenduru/gaussian-splatting/resolve/main/diff_gaussian_rasterization-0.0.0-cp310-cp310-linux_x86_64.whl
 pip install -q https://huggingface.co/camenduru/gaussian-splatting/resolve/main/simple_knn-0.0.0-cp310-cp310-linux_x86_64.whl
+pip install https://huggingface.co/camenduru/gaussian-splatting/resolve/main/diff_gaussian_rasterization-0.0.0-cp310-cp310-linux_x86_64.whl
+pip install https://huggingface.co/camenduru/gaussian-splatting/resolve/main/simple_knn-0.0.0-cp310-cp310-linux_x86_64.whl
