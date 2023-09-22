dc5e6ab131f41b2c0b313cb74eda5de05b38d000
Rajendra Singh <singh.raj1997@gmail.com>
2023-09-23 00:17:08 +0530

    Update 11_colmap.sh

--- 11_colmap.sh
+++ 11_colmap.sh
@@ -1,1 +1,2 @@
-!python convert.py -s data/ --no_gpu #should have input dir inside
+#!python convert.py -s data/ --no_gpu #should have input dir inside
+python colmap2nerf_no_gpu.py --video_in input.mp4 --video_fps 2 --run_colmap --aabb_scale 16
