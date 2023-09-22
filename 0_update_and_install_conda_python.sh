30018abaac52f2ff461bf4feb3efee13c79b999b
Rajendra Singh <singh.raj1997@gmail.com>
2023-09-22 21:52:28 +0530

    Update and rename 0_install_conda.sh to 0_update_and_install_conda_python.sh

--- 0_update_and_install_conda_python.sh
+++ 0_update_and_install_conda_python.sh
@@ -1,6 +1,11 @@
+sudo apt update -y
+sudo apt install python3-pip -y
+sudo apt install python-is-python3 -y
+
 #Downloading the latest Miniconda installer for Linux. Your architecture may vary.
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
+
 #eval "$(/Users/jsmith/miniconda/bin/conda shell.YOUR_SHELL_NAME hook)"
 #conda init
 #conda config --set auto_activate_base false
