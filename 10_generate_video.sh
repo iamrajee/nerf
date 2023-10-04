7576bf4cdc132362912c74cecc635cc0eb9ed37f
Rajendra Singh <singh.raj1997@gmail.com>
2023-10-04 21:06:12 +0530

    Update 10_generate_video.sh

--- 10_generate_video.sh
+++ 10_generate_video.sh
@@ -1,4 +1,4 @@
 sudo apt install ffmpeg -y
 conda update ffmpeg
 ffmpeg -framerate 3 -i train/ours_30000/renders/%05d.png -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -r 3 -pix_fmt yuv420p /root/nerf/gaussian-splatting/output/renders.mp4
-ffmpeg -framerate 3 -i train/ours_30000/gt/%05d.png -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -r 3 -pix_fmt yuv420p /root/nerf/gaussian-splatting/output/gt.mp4 -y
+# ffmpeg -framerate 3 -i train/ours_30000/gt/%05d.png -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -r 3 -pix_fmt yuv420p /root/nerf/gaussian-splatting/output/gt.mp4 -y
