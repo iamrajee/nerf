76003d5b451e1c7ebf4c9caccf8ced174d65a1ca
Rajendra Singh <singh.raj1997@gmail.com>
2023-10-05 23:06:58 +0530

    Update 6_configure_conda.sh

--- 6_configure_conda.sh
+++ 6_configure_conda.sh
@@ -1,6 +1,6 @@
 export PATH="/root/miniconda/bin:$PATH"
 conda init bash
 source ~/.bashrc
-cd gaussian_splatting #just precautionary, will not break the code even if we are already there in the folder
+cd gaussian-splatting #just precautionary, will not break the code even if we are already there in the folder
 conda env create --file environment.yml
 conda activate gaussian_splatting
