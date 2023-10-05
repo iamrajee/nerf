ef910217889e68b6f31b3a8b15c5b2bcf692dc97
Rajendra Singh <singh.raj1997@gmail.com>
2023-10-05 20:21:06 +0530

    Update everything.sh

--- everything.sh
+++ everything.sh
@@ -1,5 +1,5 @@
 echo "============================== START ============================="
-echo "$(date|awk '{print $5}')" "parameter_reading" >> log.txt
+echo "$(date|awk '{print $4}')" "parameter_reading" >> log.txt
 video_url="${1:-https://drive.google.com/file/d/1Ppf5Y0_0_K_yM5LVeYnI1ZHKxzD-2VKQ/view?usp=sharing}"
 iter="${2:-30000}"
 fps="${3:-3}"
@@ -7,34 +7,34 @@ save_fps="${3:-3}"
 video_name="${5:-test}" #doubt, dynamic?
 project_name="${6:-$video_name}"
 
-echo "$(date|awk '{print $5}')" "update_and_install_conda_python" > log.txt
+echo "$(date|awk '{print $4}')" "update_and_install_conda_python" > log.txt
 sudo apt update -y
 sudo apt install python3-pip -y
 sudo apt install python-is-python3 -y
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 
-echo "$(date|awk '{print $5}')" "clone_gsplat" >> log.txt
+echo "$(date|awk '{print $4}')" "clone_gsplat" >> log.txt
 git clone https://github.com/graphdeco-inria/gaussian-splatting --recursive
 
-echo "$(date|awk '{print $5}')" "install_plyfile" >> log.txt 
+echo "$(date|awk '{print $4}')" "install_plyfile" >> log.txt 
 pip install -q plyfile
 
-echo "$(date|awk '{print $5}')" "cd_gsplatting" >> log.txt 
+echo "$(date|awk '{print $4}')" "cd_gsplatting" >> log.txt 
 cd gaussian-splatting
 
-echo "$(date|awk '{print $5}')" "configure_conda" >> log.txt 
+echo "$(date|awk '{print $4}')" "configure_conda" >> log.txt 
 export PATH="/root/miniconda/bin:$PATH"
 conda init bash
 source ~/.bashrc
 conda env create --file environment.yml
 conda activate gaussian_splatting
 
-echo "$(date|awk '{print $5}')" "install_dgr_knn_whl" >> log.txt 
+echo "$(date|awk '{print $4}')" "install_dgr_knn_whl" >> log.txt 
 pip install https://huggingface.co/camenduru/gaussian-splatting/resolve/main/diff_gaussian_rasterization-0.0.0-cp310-cp310-linux_x86_64.whl
 pip install https://huggingface.co/camenduru/gaussian-splatting/resolve/main/simple_knn-0.0.0-cp310-cp310-linux_x86_64.whl
 
-echo "$(date|awk '{print $5}')" "data_prep" >> log.txt
+echo "$(date|awk '{print $4}')" "data_prep" >> log.txt
 sudo apt install colmap  -y
 mkdir -p data/$project_name/input
 sudo apt install ffmpeg -y
@@ -48,21 +48,21 @@ cd ../../../
 ffmpeg -i data/$project_name/input/* -qscale:v 1 -qmin 1 -vf fps=$fps data/$project_name/input/%04d.jpg
 python3 convert.py -s data/$project_name/ --no_gpu 
 
-echo "$(date|awk '{print $5}')" "training" >> log.txt
+echo "$(date|awk '{print $4}')" "training" >> log.txt
 export OAR_JOB_ID=$project_name #may not work
 OAR_JOB_ID=project_name #may not work
 python3 -c "import os; os.environ['OAR_JOB_ID'] =str('$project_name'); print('OAR_JOB_ID worked: ' + os.environ['OAR_JOB_ID'])"
 python3 train.py -s data/$project_name/ --iterations $iter
 output_folder=$project_name #if not then: output_folder="*"
 
-echo "$(date|awk '{print $5}')" "render" >> log.txt
+echo "$(date|awk '{print $4}')" "render" >> log.txt
 python3 render.py -m output/$output_folder
 
-echo "$(date|awk '{print $5}')" "generate rendered video" >> log.txt
+echo "$(date|awk '{print $4}')" "generate rendered video" >> log.txt
 ffmpeg -framerate $save_fps -i output/$output_folder/train/ours_$iter/renders/%05d.png -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -r 3 -pix_fmt yuv420p output/$output_folder/renders.mp4 -y
 ffmpeg -framerate $save_fps -i output/$output_folder/train/ours_$iter/gt/%05d.png -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -r 3 -pix_fmt yuv420p output/$output_folder/gt.mp4 -y
 
-echo "$(date|awk '{print $5}')" "process complete you may download the result by runing scp in local host" >> log.txt
+echo "$(date|awk '{print $4}')" "process complete you may download the result by runing scp in local host" >> log.txt
 echo "============================== END ============================="
 echo 'public-ip=[YOUR-VM-PUBLIC-IP]'
 echo 'local_download_folder="."'
